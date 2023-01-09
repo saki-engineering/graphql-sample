79fd5b6b8374d1028024eec1eeefdab8cd5a59ab
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2023-01-10 00:05:35 +0900

    dataloaderをリファクタ

--- graph/dataloader.go
+++ graph/dataloader.go
@@ -10,26 +10,25 @@ import (
 )
 
 type Loaders struct {
-	// id(string)をwhere句に使ってUserを探すため
 	UserLoader dataloader.Interface[string, *model.User]
 }
 
 func NewLoaders(Srv services.Services) *Loaders {
-	userLoader := &UserLoader{Srv: Srv}
+	userBatcher := &userBatcher{Srv: Srv}
 
 	return &Loaders{
 		// dataloader.Loader[string, *model.User]型
-		UserLoader: dataloader.NewBatchedLoader[string, *model.User](userLoader.BatchGetUsers),
+		UserLoader: dataloader.NewBatchedLoader[string, *model.User](userBatcher.BatchGetUsers),
 	}
 }
 
-type UserLoader struct {
+type userBatcher struct {
 	Srv services.Services
 }
 
 // github.com/graph-gophers/dataloader/v7 の type BatchFunc[K, V]を満たすため
 // dataloader.NewBatchedLoader関数の引数にできる
-func (u *UserLoader) BatchGetUsers(ctx context.Context, IDs []string) []*dataloader.Result[*model.User] {
+func (u *userBatcher) BatchGetUsers(ctx context.Context, IDs []string) []*dataloader.Result[*model.User] {
 	// 引数と戻り値のスライスlenは等しくする
 	result := make([]*dataloader.Result[*model.User], 0, len(IDs))
 
