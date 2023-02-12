653599d69c9479fa022c45de4bba7fae9e829801
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2023-02-12 22:43:49 +0900

    複雑度計算ロジックを改良

--- graph/complexity.go
+++ graph/complexity.go
@@ -13,5 +13,23 @@ func ComplexityConfig() internal.ComplexityRoot {
 	c.ProjectV2.Title = func(childComplexity int) int {
 		return 1
 	}
+	c.Repository.Issues = func(childComplexity int, after *string, before *string, first *int, last *int) int {
+		var cnt int
+		switch {
+		case first != nil && last != nil:
+			if *first < *last {
+				cnt = *last
+			} else {
+				cnt = *first
+			}
+		case first != nil && last == nil:
+			cnt = *first
+		case first == nil && last != nil:
+			cnt = *last
+		default:
+			cnt = 1
+		}
+		return cnt * childComplexity
+	}
 	return c
 }
